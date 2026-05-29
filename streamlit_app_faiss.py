75c72309a1803954c5120cdfd1ba0942323d78ea
youngpds2 <dvbac123@gmail.com>
2026-05-29 12:57:48 +0700

    Fix mic style and position

--- streamlit_app_faiss.py
+++ streamlit_app_faiss.py
@@ -295,7 +295,7 @@ div[data-testid="stAudioInput"] > div { overflow: hidden !important; height: 38p
 div[data-testid="stAudioInput"] button {
     width: 50px !important;
     height: 48px !important;
-    right: 20px !important;
+    right: 25px !important;
     border-radius: 8px !important;
     background: #2b313e !important;
     border: none !important;
