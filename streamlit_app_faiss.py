2cf71877c18eace7c2f00ad69c7f959cba7299ac
youngpds2 <dvbac123@gmail.com>
2026-05-29 12:49:20 +0700

    Fix mic style and position

--- streamlit_app_faiss.py
+++ streamlit_app_faiss.py
@@ -280,7 +280,7 @@ div[data-testid="stAudioInput"] {
     position: fixed !important;
     bottom: 70px !important;
     right: 50px !important;
-    width: 45px !important;
+    width: 50px !important;
     height: 48px !important;
     overflow: hidden !important;
     z-index: 1002 !important;
